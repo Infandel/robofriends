1d62d3e3b17907ff89ef920913edfd9ae6c7dbe5
Infandel <tomahawk92@gmail.com>
2021-09-21 14:25:59 +0500

    Initial commit on robofriends

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <>
+      <h3 style={{
+      color: '#9eebcf',
+      }}>
+        Infandel Co.
+      </h3>
+    </>
+  )
+}
+
+export default Footer;
